a435bc328c196986320a1bf8db6e19e79d05e9a2
ch <ch>
2019-10-02 13:47:03 +0300

    Polished code

--- toimi.py
+++ toimi.py
@@ -12,21 +12,10 @@ with open(url, 'rb') as file:
 
 img = cv2.imdecode(test, 1)
 
-
-# Test showing the image
-
-#cv2.imshow('img', img)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-
-print(img.shape)
-
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.blur(gray, (10, 10))
 
 edges = auto_canny(gray)
-
-print(np.count_nonzero(edges) / float(gray.shape[0] * gray.shape[1]))
 
 lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
 
@@ -34,8 +23,6 @@ lines = np.reshape(lines, (-1, 2))
 
 h, v = hor_vert_lines(lines)
    
-print('h: {} | v: {}'.format(h, v))
-
 points = intersections(h, v)
 
 points = cluster(points)
@@ -50,6 +37,3 @@ cv2.imwrite('new_chess1.jpg', new_img)
 arr = split_board(new_img)
 cv2.imwrite('part.jpg', arr[0])
 
-#cv2.imshow('test', new_img)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
