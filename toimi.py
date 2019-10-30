ad352a0ce28f1639d08387bebbff3891b1092026
nakkekakke <juntusen.niko@gmail.com>
2019-10-30 14:47:30 +0200

    neural network

--- toimi.py
+++ toimi.py
@@ -1,11 +1,13 @@
 import sys
 import cv2
 import numpy as np
+import torch
 
 from processor import BoardProcessor
+from model import KitModel
 
 # load input image
-url = 'images/shakki1.jpg'
+url = 'shakki1.jpg'
 with open(url, 'rb') as file:
     test = np.asarray(bytearray(file.read()))
 
@@ -21,3 +23,23 @@ if processor.processed and processor.failed:
     print(processor.error_msg)
 else:
     cv2.imwrite('images/debug/part0.jpg', processor.squares[0])
+
+# prepare the neural network
+MODEL_PATH = './model.pth'
+net = KitModel(MODEL_PATH)
+checkpoint = torch.load(MODEL_PATH)
+try:
+    checkpoint.eval()
+except AttributeError as error:
+    print(error)
+
+net.load_state_dict(checkpoint)
+net.eval()
+
+# feed squares to the neural network and get the results
+outputs = net(processor.squares)
+
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                              for j in range(4)))
